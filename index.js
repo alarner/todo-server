909e5ed260a0da7eadbea17d12d54d736f193b18
Aaron Larner <alarner@users.noreply.github.com>
2020-08-20 14:55:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+let nextId = 1;
+
+app.use(bodyParser.json());
+
+const items = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/item", (req, res) => {
+  if (!req.body.description) {
+    return res.status(400).json({ error: "Missing description" });
+  }
+  const message = {
+    id: nextId++,
+    description: req.body.description,
+    completed: !!req.body.completed,
+    createdAt: new Date(),
+  };
+  items.push(message);
+  res.json(message);
+});
+
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
